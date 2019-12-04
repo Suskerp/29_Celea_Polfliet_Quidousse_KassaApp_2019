39ca2aa2080c2736bb2e537a940f1df63e2e9aaf
r0703998 <43341159+r0703998@users.noreply.github.com>
2019-12-04 16:34:02 +0100

    kassapane bijna af

--- src/view/KassaMainPane.java
+++ src/view/KassaMainPane.java
@@ -1,10 +1,16 @@
 package view;
 
 
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import database.ArtikelDBInMemory;
+import javafx.collections.FXCollections;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import model.Artikel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Rafael Polfliet - Jef Quidousse
@@ -12,8 +18,9 @@ import javafx.scene.layout.BorderPane;
 public class KassaMainPane extends BorderPane {
 	public KassaMainPane(){
 		
-	    TabPane tabPane = new TabPane(); 	    
-        Tab kassaTab = new Tab("Kassa");
+	    TabPane tabPane = new TabPane();
+	    KassaPane kassaPane = new KassaPane();
+        Tab kassaTab = new Tab("Kassa", kassaPane.getLayout());
         ProductOverviewPane productOverviewPane = new ProductOverviewPane();
         Tab artikelTab = new Tab("Artikelen",productOverviewPane.getLayout());
         InstellingPane instellingPane = new InstellingPane();
@@ -23,6 +30,34 @@ public class KassaMainPane extends BorderPane {
         tabPane.getTabs().add(artikelTab);
         tabPane.getTabs().add(instellingTab);
         tabPane.getTabs().add(logTab);
+
+        /*GridPane gridPane = new GridPane();
+
+
+        TextField textField = new TextField();
+        textField.setPromptText("Search here!");
+
+        gridPane.getChildren().add(textField);
+
+
+
+        TableView<Artikel> table = new TableView<>();
+
+        ArtikelDBInMemory artikelDBInMemory = new ArtikelDBInMemory();
+        artikelDBInMemory.load();
+        List<Artikel> list = new ArrayList<Artikel>(artikelDBInMemory.getGesorteerdeLijst());
+        table.setItems(FXCollections.observableList(list));
+
+        TableColumn<Artikel, String> colOmschrijving = new TableColumn<>("Omschrijving");
+        colOmschrijving.setCellValueFactory(new PropertyValueFactory<>("Omschrijving"));
+        colOmschrijving.setMinWidth(50);
+
+        TableColumn<Artikel,Double> colVerkoopprijs = new TableColumn<>("Verkoopprijs");
+        colVerkoopprijs.setCellValueFactory(new PropertyValueFactory<>("Verkoopprijs") );
+        colVerkoopprijs.setMinWidth(50);
+
+        table.getColumns().addAll(colOmschrijving,colVerkoopprijs);*/
+
 
 
 	    this.setCenter(tabPane);
