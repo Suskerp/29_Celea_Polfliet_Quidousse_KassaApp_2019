36db0235fb6bc959685d838caf127b8f15424312
r0703998 <43341159+r0703998@users.noreply.github.com>
2019-12-05 12:39:56 +0100

    klantview bijna klaar

--- src/view/KlantView.java
+++ src/view/KlantView.java
@@ -1,9 +1,18 @@
 package view;
 
+import database.ArtikelDBInMemory;
+import javafx.collections.FXCollections;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Artikel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KlantView {
 	private Stage stage = new Stage();		
@@ -15,6 +24,23 @@ public class KlantView {
 		stage.setY(20);
 		Group root = new Group();
 		Scene scene = new Scene(root, 500, 500);
+
+		TableView<Artikel> table = new TableView();
+
+		TableColumn<Artikel, String> colOmschrijving = new TableColumn<>("Omschrijving");
+		colOmschrijving.setCellValueFactory(new PropertyValueFactory<>("Omschrijving"));
+		colOmschrijving.setMinWidth(50);
+
+		TableColumn<Artikel,Double> colVerkoopprijs = new TableColumn<>("Verkoopprijs");
+		colVerkoopprijs.setCellValueFactory(new PropertyValueFactory<>("Verkoopprijs") );
+		colVerkoopprijs.setMinWidth(50);
+
+		TableColumn<Artikel, Integer> colAantal = new TableColumn<>("Aantal");
+
+
+		table.getColumns().addAll(colOmschrijving,colVerkoopprijs, colAantal);
+
+		root.getChildren().add(table);
 		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();		
